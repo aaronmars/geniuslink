981942ea08d00a95b777df2b6213c45a957313bb
Aaron Mars <mars.aaron@gmail.com>
2016-02-03 14:13:04 -0800

    Refactor GeniusLink to allow multiple instances

--- geniuslink.js
+++ geniuslink.js
@@ -15,40 +15,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import User from './user';
-import search from './search';
-import settings from 'martian/lib/settings';
-export default {
+import {User} from './user';
+import {Search} from './search';
+import {Settings} from 'martian/lib/settings';
+export class GeniusLink {
 
     /**
-     * Configure the GeniusLink connection
+     * Construct and configure the GeniusLink connection
      *
      * @param {Object} configuration - An object containing other initialization values.
      */
-    configure(configuration) {
-        this.init(configuration.host);
-        if(!('token' in configuration)) {
-            throw new Error('A browser developer token must be supplied to configure the GeniusLink SDK.');
-        }
-        if(typeof configuration.token !== 'string') {
-            throw new Error('An invalid browser developer token was supplied to the GeniusLink configure() function.');
-        }
-        let token = configuration.token.trim();
-        if(token === '') {
-            throw new Error('An invalid browser developer token was supplied to the GeniusLink configure() function.');
-        }
-        settings.set('token', token);
-    },
-
-    /**
-     * Initialize GeniusLink connection
-     * @deprecated
-     * @param {String} host - MindTouch host (e.g. https://example.mindtouch.us)
-     */
-    init(host) {
-        if(!host) {
+    constructor(configuration = {}) {
+        if(!('host' in configuration)) {
             throw new Error('A MindTouch host (e.g. https://example.mindtouch.us) is required to initialize the GeniusLink connection.');
         }
+        if(!('token' in configuration)) {
+            throw new Error('A browser developer token must be supplied to configure the GeniusLink SDK.');
+        } else if(typeof configuration.token !== 'string') {
+            throw new Error('An invalid browser developer token was supplied to the GeniusLink configure() function.');
+        }
+        let host = configuration.host;
         let protocolArr = host.match(/^http(s?):/);
         let protocol = (protocolArr && Array.isArray(protocolArr)) ? protocolArr[0] : window.location.protocol;
         if(!protocolArr) {
@@ -57,8 +43,21 @@ export default {
         if(protocol === 'https:' && window.location.protocol === 'http:') {
             throw new Error('A secure HTTPS connection cannot be made from an insecure web page.');
         }
-        settings.set('host', host);
-    },
-    search: search,
-    User: User
-};
+        let token = configuration.token.trim();
+        if(token === '') {
+            throw new Error('An invalid browser developer token was supplied to the GeniusLink configure() function.');
+        }
+        this.settings = new Settings({ host: host, token: token });
+        this.userLib = new User(this.settings);
+        this.searchLib = new Search(this.settings);
+    }
+    get configuration() {
+        return this.settings.getProperties();
+    }
+    get search() {
+        return this.searchLib.search.bind(this.searchLib);
+    }
+    get User() {
+        return this.userLib;
+    }
+}
