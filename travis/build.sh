f62b961139d3f19276dff99934af5eea9efa4b1d
Andy Vaughn <andyv@mindtouch.com>
2016-04-11 16:07:34 -0700

    test s3 push

--- travis/build.sh
+++ travis/build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# check environment
-if [ "${TRAVIS_TAG}" ]; then
-    echo "travis/build.sh: releases/tags do not require a build."
-    exit 0
-fi;
-
 # gulp
 if [[ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]]; then
     node_modules/.bin/gulp test inspect
