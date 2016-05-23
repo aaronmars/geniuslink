89173ca88ed7a5e2f184e05f65771825b53e83f0
aaronmars <mars.aaron@gmail.com>
2016-05-23 14:09:16 -0700

    Integrate release 0.3.0 of martian

--- user.js
+++ user.js
@@ -15,8 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { UserManager } from 'martian/user';
-import { UserEvents } from 'martian/userEvents';
+import { UserManager } from 'mindtouch-martian/user';
+import { UserEvents } from 'mindtouch-martian/userEvents';
 
 /**
  * User management
